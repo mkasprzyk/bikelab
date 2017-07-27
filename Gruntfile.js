cc1c6c4c3057207f724426bdb6e6461a48cb29cb
mkasprzyk <mkasprzyk@szy.fr>
2017-07-27 17:26:19 +0200

    Improvements in buildsystem

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,21 +5,28 @@ module.exports = function(grunt) {
   grunt.initConfig({
     dirs: {
       dest_js: 'ui/js',
-      dest_css: 'ui/css'
+      dest_css: 'ui/css',
+      patch: 'patch',
+      node_modules: 'node_modules'
     },
     copy: {
       js: {
         files: [
 	      {
              expand: true,
-             cwd: 'node_modules/@jscad/openjscad',
-             src: 'style.css',
+             cwd: '<%= dirs.node_modules %>/@jscad/openjscad',
+             src: 'min.css',
              dest: '<%= dirs.dest_css %>',
           }
         ]
       }
+    },
+    exec: {
+      patch: "patch -N -p0 <  <%= dirs.patch %>/min_as_module.patch | true",
+      build: "browserify ui/src/bikelab.js -o <%= dirs.dest_js %>/bikelab.js"
     }
   });
+  grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.registerTask('default', ['copy']);
+  grunt.registerTask('default', ['exec', 'copy']);
 };
